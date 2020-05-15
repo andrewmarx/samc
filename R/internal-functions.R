f3f6e803f45dfbcde887073148caa9e3d9943832
Andrew Marx <andrewjmarx@gmail.com>
2020-05-15 14:53:52 -0400

    Create internal functions file

---
+++ R/internal-functions.R
@@ -0,0 +1,17 @@
+# Copyright (c) 2020 Andrew Marx. All rights reserved.
+# Licensed under GPLv3.0. See LICENSE file in the project root for details.
+#
+# This file is for internal functions. They are subject to change and should not
+# be used by users.
+#
+
+#' Template title
+#'
+#' Template description
+#'
+#' @param x Template paramater
+#'
+#' @noRd
+temp <- function(x) {
+
+}
