1e55580c95524c4b3b01bfb5d4bcdb6fa072cea9
Andrew Marx <andrewjmarx@gmail.com>
2021-03-22 23:22:04 -0400

    Add pairwise() example code

---
+++ inst/examples/pairwise.R
@@ -0,0 +1,20 @@
+library(samc)
+
+# Load example data
+res_data <- samc::ex_res_data
+abs_data <- samc::ex_abs_data
+occ_data <- samc::ex_occ_data
+
+
+# Create samc-class object
+samc_obj <- samc(res_data, abs_data, tr_fun = function(x) 1/mean(x))
+
+# pairwise() example
+pw <- pairwise(cond_passage, samc_obj, origin = 1:4, dest = 5)
+print(pw)
+
+# pairwise() example without dest
+pw <- pairwise(dispersal, samc_obj, origin = c(2, 7))
+print(pw)
+
+
