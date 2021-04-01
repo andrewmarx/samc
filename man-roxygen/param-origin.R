fab87c423afabc0833443de15ce6575ceddda32c
Andrew Marx <andrewjmarx@gmail.com>
2021-04-01 09:33:51 -0400

    Update location parameter descriptions

--- man-roxygen/param-origin.R
+++ man-roxygen/param-origin.R
@@ -1,3 +1,4 @@
-#' @param origin A positive integer representing a cell in the landscape,
-#' excluding \code{NA} cells. Corresponds to row \emph{i} of matrix \emph{P}
-#' in the \code{\link{samc-class}} object.
+#' @param origin A positive integer or named location representing a cell in the
+#' landscape. Corresponds to row \emph{i} of matrix \emph{P} in the \code{\link{samc-class}}
+#' object. When paired with the dest parameter, multiple values may be provided
+#' as a vector.
