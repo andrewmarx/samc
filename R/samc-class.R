079d7f080288079541c46dc257b003f0a91c53e0
Andrew Marx <andrewjmarx@gmail.com>
2021-04-18 15:38:46 -0400

    Setup internal classes file for samc-class usage

--- R/samc-class.R
+++ R/samc-class.R
@@ -1,6 +1,8 @@
 # Copyright (c) 2019 Andrew Marx. All rights reserved.
 # Licensed under GPLv3.0. See LICENSE file in the project root for details.
 
+#' @include internal-classes.R
+NULL
 
 #' samc class
 #'
