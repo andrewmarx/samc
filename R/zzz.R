837396773c9b8f733694a22decfc5303fcbb7e55
Andrew Marx <andrewjmarx@gmail.com>
2021-04-18 11:41:49 -0400

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -8,8 +8,10 @@
               "\n1) `latlon` is no longer required.",
               "\n2) `resistance` is deprecated. `data` should be used in its place.",
               "\n3) `p_mat` is deprecated. The P matrix is now provided via `data`.",
-              "\n4) `override` is deprecated. See the samc-class documentation for the alternative.",
-              "Previous code should continue to work except in an unlikely corner case, but version",
+              "\n4) `tr_fun` is deprecated. See the samc() function documentation for the alternative.",
+              "\n5) `directions` is deprecated. See the samc() function documentation for the alternative.",
+              "\n6) `override` is deprecated. See the samc-class documentation for the alternative.",
+              "\n\nPrevious code should continue to work except in an unlikely corner case, but version",
               "1.5.0 of the package will remove the deprecated functionality, so users",
               "should update previous work accordingly.")
   packageStartupMessage(msg)
