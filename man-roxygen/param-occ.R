8e17f477015731140f04ae9eba7e8dd9cff39713
Andrew Marx <andrewjmarx@gmail.com>
2021-04-21 17:26:16 -0400

    Update time and occ parameter descriptions

--- man-roxygen/param-occ.R
+++ man-roxygen/param-occ.R
@@ -1,5 +1,10 @@
-#' @param occ A \code{\link[raster]{RasterLayer-class}} or \code{\link[base]{matrix}}.
-#' The input type must match the input type used to create the
-#' \code{\link{samc-class}} object, and must have the same properties as the
-#' rest of the landscape data. See the \code{\link{check}} function for more
-#' details.
+#' @param occ The initial state \eqn{\psi} of the Markov chain. If the \code{\link{samc-class}}
+#' objects was constructed using map inputs, then occ must be the same type of input
+#' (either \code{\link[raster]{RasterLayer-class}} or \code{\link[base]{matrix}}),
+#' and must have the same properties as initial map inputs (see the \code{\link{check}}
+#' function).
+
+# Descriptions in
+# the Details section assume the values of \eqn{\psi} sum to 1 (\eqn{\sum_{i=1}^{n} \psi = 1}).
+# In this case, the results of analyses represent probabilities. This is not required,
+# however
