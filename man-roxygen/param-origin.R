dbb0b804931032775a7defa65ad09c22a83aa8a3
Andrew Marx <andrewjmarx@gmail.com>
2021-04-21 15:14:28 -0400

    Update origin and dest parameter descriptions

--- man-roxygen/param-origin.R
+++ man-roxygen/param-origin.R
@@ -1,4 +1,4 @@
-#' @param origin A positive integer or named location representing a cell in the
-#' landscape. Corresponds to row \emph{i} of matrix \emph{P} in the \code{\link{samc-class}}
-#' object. When paired with the dest parameter, multiple values may be provided
-#' as a vector.
+#' @param origin A positive integer or character name representing transient state
+#' \eqn{\mathit{i}}. Corresponds to row \eqn{\mathit{i}} of matrix \eqn{\mathbf{P}}
+#' in the \code{\link{samc-class}} object. When paired with the \code{dest}
+#' parameter, multiple values may be provided as a vector.
