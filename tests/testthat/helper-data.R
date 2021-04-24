3d2796d15aed6edb71688747b8022410705c54f4
Andrew Marx <andrewjmarx@gmail.com>
2021-04-24 18:45:12 -0400

    Commit temporary removal of mult absorption test stuff
    Previously commented out to fix bugs. Need to revisit

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -107,8 +107,14 @@ for(i in 1: length(masklist)) {
 
   testlist[[i]]$length <- sum(!is.na(testlist[[i]]$res))
 
+  # Version for mult absorption see mortality(samc, origin)
+  # testlist[[i]]$samc <- samc(testlist[[i]]$res,
+  #                            list(testlist[[i]]$abs * p1, testlist[[i]]$abs * p2),
+  #                            testlist[[i]]$fid,
+  #                            tr_args = list(fun = function(x) 1/mean(x), dir = 8, sym = TRUE))
+
   testlist[[i]]$samc <- samc(testlist[[i]]$res,
-                             list(testlist[[i]]$abs * p1, testlist[[i]]$abs * p2),
+                             testlist[[i]]$abs,
                              testlist[[i]]$fid,
                              tr_args = list(fun = function(x) 1/mean(x), dir = 8, sym = TRUE))
 
