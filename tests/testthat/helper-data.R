3f1000da132b5089ffca416169da0a378c992022
Andrew Marx <andrewjmarx@gmail.com>
2021-05-29 22:39:40 -0400

    Test cleanup

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -104,7 +104,7 @@ for(i in 1:length(masklist)) {
                              testlist[[i]]$abs,
                              testlist[[i]]$fid,
                              tr_args = list(fun = function(x) 1/mean(x), dir = 8, sym = TRUE))
-  print("test")
+
   testlist[[i]]$samc$abs_states <- list(testlist[[i]]$abs * p1, testlist[[i]]$abs * p2)
 
   testlist[[i]]$id <- i
