c379d0811003218fa2684bfb01a52a9274305dc3
Andrew Marx <andrewjmarx@gmail.com>
2022-12-16 20:46:44 -0500

    Documentation cleanup

--- man-roxygen/param-occ.R
+++ man-roxygen/param-occ.R
@@ -1,10 +1,4 @@
-#' @param occ The initial state \eqn{\psi} of the Markov chain. If the \code{\link{samc-class}}
-#' objects was constructed using map inputs, then occ must be the same type of input
-#' (either \code{\link[raster]{RasterLayer-class}} or \code{\link[base]{matrix}}),
-#' and must have the same properties as initial map inputs (see the \code{\link{check}}
-#' function).
-
-# Descriptions in
-# the Details section assume the values of \eqn{\psi} sum to 1 (\eqn{\sum_{i=1}^{n} \psi = 1}).
-# In this case, the results of analyses represent probabilities. This is not required,
-# however
+#' @param occ Sets the initial state \eqn{\psi} of the transients states. Input
+#' must be able to pass the \code{\link{check}} function when compared against
+#' the \code{\link{samc-class}} object. Can only contain positive finite values.
+#'
