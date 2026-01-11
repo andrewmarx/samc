5fc22ee0497d4bd96117dc1c48d5826db7c8ac44
Andrew Marx <andrewjmarx@gmail.com>
2026-01-11 08:12:07 -0500

    Documentation update
    Remove performance relative URL and just use vignette ref for CRAN check
    
    Add image alt text
    
    Set katex math renderer to get around MathML issues in reference docs
    and render issues on Chrome

--- man-roxygen/section-perf.R
+++ man-roxygen/section-perf.R
@@ -1,4 +1,4 @@
 #' @section Performance:
 #'
-#' Any relevant performance information about this function can be found in the
-#' performance vignette: \href{../doc/performance.html}{\code{vignette("performance", package = "samc")}}
+#' Performance details are in the performance vignette:
+#' \code{vignette("performance", package = "samc")}.
